38acc380873279d3241520c461ff24e72e911809
Omer Korner <omerkorner@gmail.com>
2016-10-30 01:13:45 +0100

    sqs support

--- tasks.py
+++ tasks.py
@@ -1,2 +1,16 @@
-def test_task():
+import os
+from celery import Celery
+
+ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+SECRET_KEY = os.environ['AWS_SECRET_KEY']
+
+app = Celery('tasks', broker='sqs://{}:{}@'.format(ACCESS_KEY, SECRET_KEY))
+
+@app.task
+def normal_task():
+    pass
+
+
+@app.task
+def eta_test():
     pass
